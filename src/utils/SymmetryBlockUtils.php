1d0187b5c1f5e5c18df902bf49b78df879faa916
famima65536 <famima65536@gmail.com>
2022-06-26 19:24:51 +0900

    fix original block rotations changed if they can be changed.

--- src/utils/SymmetryBlockUtils.php
+++ src/utils/SymmetryBlockUtils.php
@@ -18,6 +18,8 @@ class SymmetryBlockUtils{
 			if($facing === Facing::UP || $facing === Facing::DOWN){
 				return $block;
 			}
+			$block = clone $block;
+			/** @noinspection PhpPossiblePolymorphicInvocationInspection */
 			$block->setFacing(Facing::rotateY(Facing::rotateY($facing, true), true));
 		}
 		return $block;
@@ -26,11 +28,13 @@ class SymmetryBlockUtils{
 	public static function mirrorBlock(Block $block, int $axis) : Block{
 		if(method_exists($block, "getFacing") && method_exists($block, "setFacing")){
 			$facing = $block->getFacing();
+			$block = clone $block;
 			$facing ^= match ($axis){
 				Axis::X => ($facing & Facing::WEST) === Facing::WEST,
 				Axis::Z => ($facing & Facing::NORTH) === Facing::NORTH,
 				default => throw new LogicException("invalid axis")
 			};
+			/** @noinspection PhpPossiblePolymorphicInvocationInspection */
 			$block->setFacing($facing);
 		}
 		return $block;
